4e182c5e56b7d69486341837c7f60870b77d9549
fionn mcgoldrick <fionnmc123@gmail.com>
2026-02-25 20:42:38 +0000

    Add player movement: - Add player jump, sprint and walk

---
+++ Assets/Player.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 0.5f;
+    [SerializeField] private float jumpForce = 3f;
+
+    [SerializeField] private float lineDistance = 1f;
+
+    [SerializeField] private LayerMask whatIsGround;
+
+    [SerializeField] private bool isGrounded;
+
+    private Rigidbody2D rb;
+    private float xInput;
+
+    private void Awake()
+    {
+        rb = GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        HandleCollision();
+    }
+    
+    private void HandleInput()
+    {
+        HandleWalk();
+        HandleSprint();
+        HandleJump();
+    }
+
+    private void HandleWalk()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void HandleSprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && rb.linearVelocity.x != 0 && isGrounded)
+            moveSpeed = 4;
+        else 
+            moveSpeed = 3;
+    }
+
+    private void HandleJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, lineDistance, whatIsGround);
+    }
+
+    private void OnDrawGizmos() => Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -lineDistance));
+    
+
+}
